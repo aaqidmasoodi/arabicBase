32ec6bf95387852e45545154f71ed4e15ede3d1a
Aaqid Masoodi <aaqidmasoodi@icloud.com>
2025-12-02 19:04:56 +0000

    dev: added husky pre commit checks

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'; // Trigger lint-staged
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
